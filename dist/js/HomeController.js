ae6a18088491cfc65d3e25132f07bd2bba1aa605
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2015-07-08 01:40:28 -0400

    added headroom module with animations.

--- dist/js/HomeController.js
+++ dist/js/HomeController.js
@@ -3,4 +3,24 @@ app.controller('HomeController', ['$scope', function($scope) {
         'Jeffrey',
         'Billy'
     ];
+
+    // By default the 'text' property will be used as the display text in the dropdown entry.
+    // All options that are not dividers must have a 'text' property.
+    // You can specify a different property name in place of 'text' via the dropdown-item-label attribute.
+    // A divider with a 'text' property will also be non-selectable.
+    //
+    // If an options object has an 'href' property set, then that dropdown entry
+    //   will behave as a link and cannot be selected.
+    $scope.ddSelectOptions = [
+        {
+            text: 'Jeffrey',
+           href: "#/details/Jeffrey"
+        },
+        {
+            text: 'Billy',
+            href: "#/details/Billy"
+        }
+    ];
+
+    $scope.ddSelectSelected = {}; // Must be an object
 }]);
--- dist/js/app.js
+++ dist/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('myApp', ['ngRoute']);
+var app = angular.module('myApp', ['ngRoute', 'ngDropdowns', 'headroom']);
 
 app.config(function ($routeProvider) {
     $routeProvider
