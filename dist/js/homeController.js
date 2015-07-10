d7aefe83cee9550fca46ad5a38a746ceb67a1721
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2015-07-09 23:20:41 -0400

    Removed the people dropdown ont he home page and replaced it with ordinary links.

--- dist/js/homeController.js
+++ dist/js/homeController.js
@@ -1,12 +1,5 @@
 app.controller('homeController', ['$scope', 'user', function ($scope, user) {
-    // By default the 'text' property will be used as the display text in the dropdown entry.
-    // All options that are not dividers must have a 'text' property.
-    // You can specify a different property name in place of 'text' via the dropdown-item-label attribute.
-    // A divider with a 'text' property will also be non-selectable.
-    //
-    // If an options object has an 'href' property set, then that dropdown entry
-    //   will behave as a link and cannot be selected.
-    $scope.ddSelectOptions = [
+    $scope.people = [
         {
             text: 'Jeffrey',
             href: "#/details/Jeffrey"
@@ -16,6 +9,4 @@ app.controller('homeController', ['$scope', 'user', function ($scope, user) {
             href: "#/details/Billy"
         }
     ];
-
-    $scope.ddSelectSelected = {}; // Must be an object
 }]);
