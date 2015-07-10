7e68e20a7563647fb746c8a51631bbbfca18a24b
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2015-07-09 23:02:02 -0400

    Added a link tothte icon in the header back to the home.

--- dist/js/app.js
+++ dist/js/app.js
@@ -12,6 +12,10 @@ app.config(function ($stateProvider, $urlRouterProvider) {
             controller: 'ChartsController',
             templateUrl: 'views/chartsView.html'
         })
+        .state('lorem', {
+            url: '/lorem',
+            templateUrl: 'views/loremView.html'
+        })
         .state('details', {
             url: '/details/:name',
             controller: 'DetailsController',
@@ -19,18 +23,3 @@ app.config(function ($stateProvider, $urlRouterProvider) {
         });
     $urlRouterProvider.otherwise('/');
 });
-//
-//app.run(function ($templateCache) {
-//    $templateCache.put('ngDropdowns/templates/dropdownMenu.html', [
-//        '<span class="wrap-dd-select">',
-//        '<span class="selected">{{dropdownModel[labelField]}}</span>',
-//        '<ul class="custom-dropdown">',
-//        '<li ng-repeat="item in dropdownSelect"',
-//        ' class="dropdown-item"',
-//        ' dropdown-select-item="item"',
-//        ' dropdown-item-label="labelField">',
-//        '</li>',
-//        '</ul>',
-//        '</span>'
-//    ].join(''));
-//});
