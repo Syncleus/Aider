8e1f1d6a268e92987656e59382e12ee3dfe95eff
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2015-07-11 17:44:57 -0400

    Added states and value ranges to the data model details.

--- dist/js/dataModelController.js
+++ dist/js/dataModelController.js
@@ -4,73 +4,85 @@ app.controller('dataModelController', ['$scope', function ($scope) {
             "key": "educationMax",
             "description": "Highest level of education completed",
             "detailsVisible" : false,
-            "type": "discrete"
+            "type": "discrete",
+            "states": ["none", "High School", "Associates", "Bachlors", "Masters", "PhD"]
         },
         {
             "key": "personalIncomeLevel",
             "description": "Individual's average income per year",
             "detailsVisible" : false,
-            "type": "number"
+            "type": "number",
+            "rangeMin": 0,
         },
         {
             "key": "householdIncomeLevel",
-            "description": "Household average income per year",
+            "description": "Household's average income per year",
             "detailsVisible" : false,
-            "type": "number"
+            "type": "number",
+            "rangeMin": 0,
         },
         {
             "key": "race",
             "description" : "The race the individual identifies with",
             "detailsVisible" : false,
-            "type": "discrete"
+            "type": "discrete",
+            "states": ["Caucasion", "African", "Hispanic", "Native American", "Other"]
         },
         {
             "key": "sex",
             "description" : "Self-identified sex of the individual",
             "detailsVisible" : false,
-            "type": "discrete"
+            "type": "discrete",
+            "states": ["male", "female", "intersexed"]
         },
         {
             "key": "politicalParty",
             "description": "Registered political party affiliation",
             "detailsVisible" : false,
-            "type": "discrete"
+            "type": "discrete",
+            "states": ["Democrat", "republican", "Libertarian", "Independent", "Other"]
         },
         {
             "key": "birthYear",
             "description": "The year the individual was born",
             "detailsVisible" : false,
-            "type": "number"
+            "type": "number",
+            "rangeMax": 2015
         },
         {
             "key": "profession",
             "description": "Primary field of employment",
             "detailsVisible" : false,
-            "type": "discrete"
+            "type": "discrete",
+            "states": ["Health Care", "Science", "Information Technology", "Service Industry", "Other"]
         },
         {
             "key": "countryOfResidency",
             "description": "Country the individual spends most of their time per year",
             "detailsVisible" : false,
-            "type": "discrete"
+            "type": "discrete",
+            "states": ["United Stated of America", "Canada", "Mexico", "Other"]
         },
         {
             "key": "childrenCount",
             "description": "Number of children the individual has as a dependency",
             "detailsVisible" : false,
-            "type": "number"
+            "type": "number",
+            "rangeMin": 0,
         },
         {
             "key": "maritalStatus",
             "description": "The individuals current relationship status",
             "detailsVisible" : false,
-            "type": "discrete"
+            "type": "discrete",
+            "states": ["Single", "Married", "Seperated", "Widowed", "In relationship"]
         },
         {
             "key": "citizenship",
             "description": "Indicates the individuals citizenship status",
             "detailsVisible" : false,
-            "type": "discrete"
+            "type": "discrete",
+            "states": ["Citizen", "Green Card Holder", "Visa Holder", "Other"]
         }
     ];
 
