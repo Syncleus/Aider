f9dacf898fc60349879181d66ab02c01a34837bf
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2015-07-11 16:39:47 -0400

    Added actual useful information tot the data model details.

--- dist/js/dataModelController.js
+++ dist/js/dataModelController.js
@@ -2,51 +2,75 @@ app.controller('dataModelController', ['$scope', function ($scope) {
     $scope.datapoints = [
         {
             "key": "educationMax",
-            "description": "Highest level of education completed"
+            "description": "Highest level of education completed",
+            "detailsVisible" : false,
+            "type": "discrete"
         },
         {
             "key": "personalIncomeLevel",
-            "description": "Individual's average income per year"
+            "description": "Individual's average income per year",
+            "detailsVisible" : false,
+            "type": "number"
         },
         {
             "key": "householdIncomeLevel",
-            "description": "Household average income per year"
+            "description": "Household average income per year",
+            "detailsVisible" : false,
+            "type": "number"
         },
         {
             "key": "race",
-            "description" : "The race the individual identifies with"
+            "description" : "The race the individual identifies with",
+            "detailsVisible" : false,
+            "type": "discrete"
         },
         {
             "key": "sex",
-            "description" : "Self-identified sex of the individual"
+            "description" : "Self-identified sex of the individual",
+            "detailsVisible" : false,
+            "type": "discrete"
         },
         {
             "key": "politicalParty",
-            "description": "Registered political party affiliation"
+            "description": "Registered political party affiliation",
+            "detailsVisible" : false,
+            "type": "discrete"
         },
         {
             "key": "birthYear",
-            "description": "The year the individual was born"
+            "description": "The year the individual was born",
+            "detailsVisible" : false,
+            "type": "number"
         },
         {
             "key": "profession",
-            "description": "Primary field of employment"
+            "description": "Primary field of employment",
+            "detailsVisible" : false,
+            "type": "discrete"
         },
         {
             "key": "countryOfResidency",
-            "description": "Country the individual spends most of their time per year"
+            "description": "Country the individual spends most of their time per year",
+            "detailsVisible" : false,
+            "type": "discrete"
         },
         {
             "key": "childrenCount",
-            "description": "Number of children the individual has as a dependency"
+            "description": "Number of children the individual has as a dependency",
+            "detailsVisible" : false,
+            "type": "number"
         },
         {
             "key": "maritalStatus",
-            "description": "The individuals current relationship status"
+            "description": "The individuals current relationship status",
+            "detailsVisible" : false,
+            "type": "discrete"
         },
         {
             "key": "citizenship",
-            "description": "Indicates the individuals citizenship status"
+            "description": "Indicates the individuals citizenship status",
+            "detailsVisible" : false,
+            "type": "discrete"
         }
     ];
 }]);
