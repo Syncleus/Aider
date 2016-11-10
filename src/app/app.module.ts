5e1221cd24e5b3bfb807d941d25140c73340a260
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2016-11-10 02:30:49 -0500

    Got a basic page working.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {MaterialModule} from '@angular/material';
+import {AppComponent, DialogContent} from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot(),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, DialogContent],
+  entryComponents: [DialogContent],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
